9f8580947432d0f5123304d5a443bbdd1786d52d
Robert Schönthal <caziel@gmx.net>
2011-05-04 17:07:00 +0200

    playing with esi

--- web/public_dev.php
+++ web/public_dev.php
@@ -2,8 +2,11 @@
 
 require_once __DIR__.'/../app/bootstrap.php';
 require_once __DIR__.'/../app/AppKernel.php';
+require_once __DIR__.'/../app/AppCache.php';
 
 use Symfony\Component\HttpFoundation\Request;
 
 $kernel = new AppKernel('dev', true);
+//$kernel = new AppCache(new AppKernel('dev', true));
+
 $kernel->handle(Request::createFromGlobals())->send();
